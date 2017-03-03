42248ef10a38692bb020f90175ff6de155042405
shahnawf <snawaz760@yahoo.com>
2017-03-03 18:27:28 -0500

    Added new enum for member ship type

---
+++ shack-data/src/main/java/com/gaming/shack/data/enums/MembershipType.java
@@ -0,0 +1,18 @@
+package com.gaming.shack.data.enums;
+
+/**
+ * 
+ * @author shahnawf
+ *
+ */
+public enum MembershipType {
+	LIFETIME(1) ,
+	VIP3(2) , 
+	VIP6(3) ;
+	
+	private int typeValue ;
+	
+	MembershipType(int typeValue) {
+		this.typeValue = typeValue ;
+	}
+}
